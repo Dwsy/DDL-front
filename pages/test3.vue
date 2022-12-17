caeb23edbef3341dfd7b4af791a136eb78f1f26e
Dwsy <1521986032@qq.com>
2022-12-18 00:34:23 +0800

    heatmap

--- pages/test3.vue
+++ pages/test3.vue
@@ -1,30 +1,8 @@
-<template>
-  <v-select
-    prepend-icon="mdi-code-tags"
-    label="代码高亮风格Base16"
-    class="mx-2 mt-5"
-    :items="HighlightStyleBase16NameList"
-    :item-title="(item) => x(item)"
-    :item-value="123"
-    variant="underlined"
-    v-model="highlightStyle"
-  ></v-select>
-</template>
-
 <script setup lang="ts">
-import { clog } from '~/utils/clog'
-import {
-  changeHighlightStyle,
-  HighlightStyleNameList,
-  HighlightStyleBase16NameList,
-} from '~~/constant/highlightStyleList'
-import { ref } from 'vue'
-
-const x = (item) => {
-  clog(item)
-  return '123'
-}
-const highlightStyle = ref()
+import {MlHeatmap} from 'ml-heatmap'
+import 'ml-heatmap/dist/style.css'
 </script>
 
-<style scoped></style>
+<template>
+  <ml-heatmap :data="[]" />
+</template>
